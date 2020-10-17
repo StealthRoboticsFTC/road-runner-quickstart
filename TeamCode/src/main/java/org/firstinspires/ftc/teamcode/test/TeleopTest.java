23f0cd073c9c7f3661fa2200cb99fa951fa4b027
Tyler <berlity0611@gmail.com>
2020-10-17 10:47:59 -0500

    Added New stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/TeleopTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/TeleopTest.java
@@ -1,4 +1,36 @@
-package org.firstinspires.ftc.teamcode.test;
+package org.firstinspires.ftc.teamcode;
 
-public class TeleopTest {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+@TeleOp
+public class TeleopTest extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        waitForStart();
+
+
+        DcMotor leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        DcMotor leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        DcMotor rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        DcMotor rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        while (opModeIsActive()) {
+            double forwardPower = -gamepad1.left_stick_y;
+            double straife = gamepad1.left_stick_x;
+            double turn = gamepad1.right_stick_x;
+
+            leftFront.setPower(forwardPower + straife + turn);
+            leftBack.setPower(forwardPower - straife + turn);
+            rightFront.setPower(forwardPower - straife - turn);
+            rightBack.setPower(straife + forwardPower - turn);
+
+
+        }
+    }
+
 }
