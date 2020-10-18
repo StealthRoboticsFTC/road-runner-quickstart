d33041581b59803627a1af4c7a6b153999aa8b14
Tyler <berlity0611@gmail.com>
2020-10-17 22:35:19 -0500

    add teleop intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/TeleopTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/TeleopTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.test;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -10,26 +10,35 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 public class TeleopTest extends LinearOpMode {
     @Override
     public void runOpMode() {
-        waitForStart();
-
-
         DcMotor leftFront = hardwareMap.get(DcMotor.class, "leftFront");
         DcMotor leftBack = hardwareMap.get(DcMotor.class, "leftBack");
         DcMotor rightFront = hardwareMap.get(DcMotor.class, "rightFront");
         DcMotor rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+
+        DcMotor intakeLeft = hardwareMap.get(DcMotor.class, "intakeLeft");
+        DcMotor intakeRight = hardwareMap.get(DcMotor.class, "intakeRight");
+
+        intakeRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
         leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
         leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
         while (opModeIsActive()) {
             double forwardPower = -gamepad1.left_stick_y;
             double straife = gamepad1.left_stick_x;
             double turn = gamepad1.right_stick_x;
+
+            double intake = gamepad1.right_trigger - gamepad1.left_trigger;
 
             leftFront.setPower(forwardPower + straife + turn);
             leftBack.setPower(forwardPower - straife + turn);
             rightFront.setPower(forwardPower - straife - turn);
             rightBack.setPower(straife + forwardPower - turn);
 
-
+            intakeRight.setPower(intake);
+            intakeLeft.setPower(intake);
         }
     }
 
