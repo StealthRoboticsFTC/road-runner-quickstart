858d567391aa74e1b91b84cea7f057d62753028a
tyler0611 <berlity0611@gmail.com>
2020-10-24 10:47:55 -0500

    Added Folders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/TeleopTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/TeleopTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.test;
+package org.firstinspires.ftc.teamcode.main;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
