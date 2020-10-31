d8c80065cb0438d7d3c627eebc422cb13ad95ebe
Johnathan Vogt <johnathanvogt92@gmail.com>
2020-10-31 13:24:08 -0500

    Added new files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/TeleOpFinal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/TeleOpFinal.java
@@ -1,5 +1,48 @@
 package org.firstinspires.ftc.teamcode.subsystem;
 
-public class TeleOpFinal {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@TeleOp
+public class TeleOpFinal extends LinearOpMode {
+    private WobbleArm wobbleArm;
+    private Shooter shooter;
+    private Intake intake;
+
+    @Override
+    public void runOpMode() {
+
+        wobbleArm = new WobbleArm(hardwareMap);
+        shooter = new Shooter(hardwareMap);
+        intake = new Intake(hardwareMap);
+
+        waitForStart();
+
+        boolean isYDown = false;
+        boolean isBDown = false;
+
+        while (!isStopRequested()) {
+            if (gamepad1.b && !isYDown) {
+
+                if (!wobbleArm.isArmDown()) {
+                    wobbleArm.moveDown();
+                } else {
+                    wobbleArm.moveUp();
+                }
+
+            }
+
+            if (gamepad1.y && !isBDown) {
+                if (!wobbleArm.isGripOpen()) {
+                    wobbleArm.gripClose();
+                } else {
+                    wobbleArm.gripOpen();
+                }
+            }
+
+            isYDown = gamepad1.y;
+            isBDown = gamepad1.b;
+
+        }
+    }
 }
