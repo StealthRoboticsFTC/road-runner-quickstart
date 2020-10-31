d44089979d2241a2896b41419e5beb37e2a942d4
Johnathan Vogt <johnathanvogt92@gmail.com>
2020-10-31 13:43:21 -0500

    Added new files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Shooter.java
@@ -1,15 +1,27 @@
 package org.firstinspires.ftc.teamcode.subsystem;
 
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.util.InterpLUT;
+import java.util.Arrays;
 
 public class Shooter {
     private HardwareMap hardwareMap;
     private DcMotor frontShooter;
     private DcMotor backShooter;
     private Servo shooterArm;
+    private Servo shooterFlap;
+
+    private SampleMecanumDrive drive;
+
+    private InterpLUT flapPositionTable = InterpLUT.createLUT(
+            Arrays.asList(0.0), Arrays.asList(0.0)
+    );
 
     enum State {
         OFF,
@@ -29,17 +41,21 @@ public class Shooter {
     private static double ARM_OUT_TIME = 0.1;
     private static double ARM_IN_TIME = 0.05;
 
+    public static Vector2d GOAL_POSITION = new Vector2d(124, 106);
+
     private int shotsRemaining = 0;
     private boolean armIsIn = true;
     private ElapsedTime armWaitTime = new ElapsedTime();
 
     private ElapsedTime rampTime = new ElapsedTime();
 
-    public Shooter(HardwareMap hardwareMap) {
+    public Shooter(HardwareMap hardwareMap, SampleMecanumDrive drive) {
         this.hardwareMap = hardwareMap;
         this.frontShooter = hardwareMap.get(DcMotor.class, "frontShooter");
         this.backShooter = hardwareMap.get(DcMotor.class, "backShooter");
         this.shooterArm = hardwareMap.get(Servo.class, "shooterArm");
+        this.shooterFlap = hardwareMap.get(Servo.class, "shooterFlap");
+        this.drive = drive;
         this.stop();
         this.moveArmIn();
     }
@@ -56,8 +72,10 @@ public class Shooter {
                 }
                 break;
             case RUNNING:
+                updateFlap();
                 break;
             case FIRING:
+                updateFlap();
                 if (armIsIn && armWaitTime.seconds() >= ARM_IN_TIME) {
                     moveArmOut();
                     shotsRemaining--;
@@ -108,4 +126,11 @@ public class Shooter {
         shooterArm.setPosition(MAX_ARM_POSITION);
         armIsIn = false;
     }
+
+    private void updateFlap() {
+        double distance = drive.getPoseEstimate().vec().minus(GOAL_POSITION).norm();
+        double flapPosition = flapPositionTable.get(distance);
+        shooterFlap.setPosition(flapPosition);
+    }
+
 }
